930739e69dd0441e70e71d942010df66a7d620af
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-09-30 18:22:20 -0300

    Create ConfirmPurchaseModal component

---
+++ src/Cart/ConfirmPurchaseModal.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import { useDispatchContext } from './../context/Cart';
+
+export default function ConfirmedPurchaseModal({ close }) {
+  const dispatch = useDispatchContext()
+  
+  const handleCleanCart = () => {
+    dispatch({ type: 'clean' })
+    close()
+  }
+  
+  return (
+    <div className='bg-black/50 inset-0 fixed grid place-content-center'>
+      <div className='p-5 rounded bg-white border-4 border-green-500 flex flex-col items-center gap-4'>
+      <img className='w-32 opacity-50' src='/src/assets/cart-check.svg' alt='Purchase Confirmed' />
+        <p className='text-xl'>Your purchase has been confirmed</p>
+        <button className='bg-green-500 px-8 py-2 text-white' onClick={handleCleanCart}>
+          Ok
+        </button>
+      </div>
+    </div>
+  )
+}
+
+ConfirmedPurchaseModal.propTypes = {
+  close: PropTypes.func.isRequired,
+}
