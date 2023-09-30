44275829930907126539880d6a08a0c5b1c63860
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-09-30 18:26:35 -0300

    Create Error component

---
+++ src/Shop/Error.jsx
@@ -0,0 +1,24 @@
+import { useNavigate } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+export default function ErrorMessage({ error }) {
+  const navigate = useNavigate()
+  return (
+    <div className='h-[100svh] flex flex-col items-center justify-center'>
+      <img className='w-32 opacity-60' src='/src/assets/cart-error.svg' alt='Error' />
+      <p className='text-2xl'>{error}</p>
+      <p className='text-2xl'>Something went wrong</p>
+      <button className='px-4 py-2 bg-black text-white mt-4 mb-2' onClick={() => navigate(0)}>
+        Refresh the page
+      </button>
+      <p>or try again later</p>
+    </div>
+  )
+}
+
+ErrorMessage.propTypes = {
+  error: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string
+  ]).isRequired
+}
